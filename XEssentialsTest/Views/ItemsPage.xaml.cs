2cf9361f30cfeb2ed58c800cb1457e97cb6c5219
José Luis Orihuela Conde <joseluis@kite.net.ar>
2020-07-22 15:02:35 -0300

    Agregar archivos de proyecto.

---
+++ XEssentialsTest/Views/ItemsPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.ComponentModel;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+using XEssentialsTest.Models;
+using XEssentialsTest.Views;
+using XEssentialsTest.ViewModels;
+
+namespace XEssentialsTest.Views
+{
+    public partial class ItemsPage : ContentPage
+    {
+        ItemsViewModel _viewModel;
+
+        public ItemsPage()
+        {
+            InitializeComponent();
+
+            BindingContext = _viewModel = new ItemsViewModel();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            _viewModel.OnAppearing();
+        }
+    }
+}
---
+++ XEssentialsTest/Views/NewItemPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+using XEssentialsTest.Models;
+using XEssentialsTest.ViewModels;
+
+namespace XEssentialsTest.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
